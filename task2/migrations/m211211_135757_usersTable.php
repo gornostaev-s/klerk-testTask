329932be6407f140d0c83a4c95b8d3711fcb69bb
gornostaev-s <gornostaev.s19@gmail.com>
2021-12-11 17:51:14 +0300

    add users table

--- task2/migrations/m211211_135757_usersTable.php
+++ task2/migrations/m211211_135757_usersTable.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\db\Migration;
+use yii\db\Schema;
 
 /**
  * Class m211211_135757_usersTable
@@ -12,7 +13,18 @@ class m211211_135757_usersTable extends Migration
      */
     public function safeUp()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
 
+        $this->createTable('{{%users}}',[
+            'id' => Schema::TYPE_PK,
+            'name' => Schema::TYPE_STRING . ' NOT NULL',
+            'surname' => Schema::TYPE_STRING . ' NOT NULL',
+            'patronymic' => Schema::TYPE_STRING . ' NOT NULL',
+            'updated_by' => Schema::TYPE_DATETIME . ' NOT NULL',
+        ], $tableOptions);
     }
 
     /**
@@ -20,7 +32,7 @@ class m211211_135757_usersTable extends Migration
      */
     public function safeDown()
     {
-        echo "m211211_135757_usersTable cannot be reverted.\n";
+        $this->dropTable('{{%users}}');
 
         return false;
     }
