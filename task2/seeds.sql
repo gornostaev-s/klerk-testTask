9a9d6a13c018b2d51fe712cdaa9343e13d6bf02b
root <root@ubuntu>
2021-12-15 18:08:44 +0000

    task2 add seeds & upd readme

---
+++ task2/seeds.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO `phones` (`id`, `user_id`, `phone`) VALUES
+(1, 1, '8985999999999'),
+(2, 1, '79999999999'),
+(3, 2, '8985999999999'),
+(4, 2, '77777777777');
+
+
+INSERT INTO `users` (`id`, `name`, `surname`, `patronymic`, `updated_by`) VALUES
+(1, 'name', 'surname', 'patronymic', '2021-12-15 18:01:38'),
+(2, 'Имя', 'Фамилия', 'Отчество', '2021-12-15 18:01:40');
